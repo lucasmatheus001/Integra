476b421efce211d783ca00b5678e617fdfafe02f
lucasmatheus001 <lucasmatheussm22@gmail.com>
2024-07-17 11:29:54 -0300

    adicionando cargo a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  enum role: { admin: 0, otaku: 1}
+  enum active: { inactive: 0, active: 1}
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
--- db/migrate/20240717135525_create_users.rb
+++ db/migrate/20240717135525_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :name
       t.datetime :birthday
+      t.integer "role", default: 1
+      t.integer "active", default: 1
 
       t.timestamps
     end
