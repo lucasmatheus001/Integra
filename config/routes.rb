78b1b15477df45d2a0daded6b63900cc7f98a3fe
lucasmatheus001 <lucasmatheussm22@gmail.com>
2024-07-17 14:00:21 -0300

    rotas iniciais devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,7 +7,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  root "users#index"
+  root "home#index"
+  resources :users
+  resources :payments
   # Defines the root path route ("/")
   # root "posts#index"
 end
