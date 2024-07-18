7e78e83e05d108b137f313998d5637f676768c79
lucasmatheus001 <lucasmatheussm22@gmail.com>
2024-07-18 00:26:51 -0300

    adicionando index de pagamentos

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,7 @@
 class PaymentsController < ApplicationController
   protect_from_forgery except: :process_payment
   def index
+    @payments = current_user.role == 'admin' ? Payment.all : current_user.payments
   end
 
   def process_payment
@@ -28,15 +29,23 @@ class PaymentsController < ApplicationController
 
       if payment['status'] == 'approved'
         # render json: { status: 'success', message: 'Pagamento aprovado!' }
+        Payment.create(user_id: current_user.id, amount: transaction_amount, approved: true)
         redirect_to payments_sucess_path
       else
         # render json: { status: 'error', message: payment['status_detail'] }
+        Payment.create(user_id: current_user.id, amount: transaction_amount, approved: false)
         redirect_to payments_fail_path
       end
     rescue StandardError => e
       render json: { status: 'error', message: e.message }
       redirect_to payments_fail_path
     end
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:amount, :email, :user_id)
   end
 
 end
