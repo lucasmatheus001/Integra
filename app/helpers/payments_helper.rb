76d49aa6af74da408595edc1a7b30eaf4375e9f2
lucasmatheus001 <lucasmatheussm22@gmail.com>
2024-07-18 19:11:43 -0300

    format data

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,2 +1,6 @@
 module PaymentsHelper
+  def post_date(date)
+    # formatting date: Aug, 31 2007 - 9:55PM
+    date.strftime("%d - %m - %Y - %H:%M")
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def find_user_by_id(id)
+    user = User.where(id: id).first
+    user.present? ? user.name : 'Usuário não encontrado'
+  end
 end
