53e0de819ec68f4db2b161df2e45589dfa386455
lucasmatheus001 <lucasmatheussm22@gmail.com>
2024-07-19 17:28:05 -0300

    refact bug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -40,31 +40,25 @@ class PaymentsController < ApplicationController
 
     begin
       payment_response = sdk.payment.create(payment_data)
+      # raise payment_response.inspect
       payment = payment_response[:response]
-      data_string = payment["cause"][0]["data"]
-      date_part = data_string.split(';').first
 
-      datetime_object = DateTime.strptime(date_part, '%d-%m-%YT%H:%M:%S%Z')
-
-      formatted_date = datetime_object.strftime('%Y-%m-%d %H:%M:%S.%N %z')
-
+      data_string = payment["date_created"].to_time
 
       if payment['status'] == 'approved'
         # render json: { status: 'success', message: 'Pagamento aprovado!' }
-        @new_payment = Payment.new(user_id: current_user.id, amount: payment_data[:transaction_amount], approved: true, created_at: formatted_date, updated_at: formatted_date )
+        @new_payment = Payment.new(user_id: current_user.id, amount: payment_data[:transaction_amount], approved: true, created_at: data_string, updated_at: data_string )
         @new_payment.save
         redirect_to payments_success_path
       else
         # render json: { status: 'error', message: payment['status_detail'] }
-        @new_payment = Payment.new(user_id: current_user.id, amount: payment_data[:transaction_amount], approved: false, created_at: formatted_date, updated_at: formatted_date)
+        @new_payment = Payment.new(user_id: current_user.id, amount: payment_data[:transaction_amount], approved: false, created_at: data_string, updated_at: data_string)
         @new_payment.save
-
-        # binding.pry
-
         redirect_to payments_fail_path
       end
     rescue StandardError => e
-      render json: { status: 'error', message: e.message }
+      logger.info e.inspect
+      # render json: { status: 'error', message: e.message }
       redirect_to payments_fail_path
     end
   end
